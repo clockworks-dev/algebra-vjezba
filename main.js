36f8bdf105465878fe7d3564b172a04d9495d6f2
GaStats <mkziba@gmail.com>
2026-08-31 20:42:43 +0200

    Dodan bootstrap

--- main.js
+++ main.js
@@ -14,7 +14,50 @@ function renderPokemon(pokemon) {
     const template = Handlebars.compile(source);
 
     document.getElementById("pokemon-results").innerHTML = template({ pokemon });
+
+    const popoverElements = document.querySelectorAll('[data-bs-toggle="popover"]');
+
+    popoverElements.forEach(function (popoverElement) {
+        new bootstrap.Popover(popoverElement, {
+            template:
+                '<div class="popover" role="tooltip">' +
+                '<div class="popover-arrow"></div>' +
+                '<h3 class="popover-header"></h3>' +
+                '<div class="popover-close" role="button" tabindex="0" aria-label="Zatvori popover">&times;</div>' +
+                '<div class="popover-body"></div>' +
+                '</div>'
+        });
+
+        popoverElement.addEventListener("show.bs.popover", function () {
+            popoverElements.forEach(function (otherPopoverElement) {
+                if (otherPopoverElement !== popoverElement) {
+                    bootstrap.Popover.getInstance(otherPopoverElement).hide();
+                }
+            });
+        });
+    });
 }
+
+document.addEventListener("click", function (event) {
+    const closeButton = event.target.closest(".popover-close");
+
+    if (closeButton) {
+        const openPopover = closeButton.closest(".popover");
+        const trigger = document.querySelector('[aria-describedby="' + openPopover.id + '"]');
+
+        if (trigger) {
+            bootstrap.Popover.getInstance(trigger).hide();
+        }
+
+        return;
+    }
+
+    if (!event.target.closest('[data-bs-toggle="popover"], .popover')) {
+        document.querySelectorAll('[data-bs-toggle="popover"]').forEach(function (popoverElement) {
+            bootstrap.Popover.getInstance(popoverElement).hide();
+        });
+    }
+});
 
 function getYellowPokemon() {
     const request = new XMLHttpRequest();
